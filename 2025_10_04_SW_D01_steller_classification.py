13016e5188fbe482e3ff8428cac578c28100c780
Tracy_Chacon <tracy.chacon.00@gmail.com>
2025-10-05 01:46:46 -0500

    chore: rename 2025_10_04 to match naming convention

---
+++ 2025_10_04_SW_D01_steller_classification.py
@@ -0,0 +1,48 @@
+# Space Week Day 1: Stellar Classification
+# October 4th marks the beginning of World Space Week. The next seven days will bring you astronomy-themed coding challenges.
+
+# For today's challenge, you are given the surface temperature of a star in Kelvin (K) and need to determine its stellar classification based on the following ranges:
+
+# "O": 30,000 K or higher
+
+# "B": 10,000 K - 29,999 K
+
+# "A": 7,500 K - 9,999 K
+
+# "F": 6,000 K - 7,499 K
+
+# "G": 5,200 K - 5,999 K
+
+# "K": 3,700 K - 5,199 K
+
+# "M": 0 K - 3,699 K
+
+# Return the classification of the given star.
+
+def classification(temp: int) -> str:
+
+    match temp:
+        case kelvin if kelvin >= 30000:
+            return "O" 
+        case kelvin if 10000 <= kelvin <= 29999:
+            return "B" 
+        case kelvin if 7500 <= kelvin <= 9999:
+            return "A" 
+        case kelvin if 6000 <= kelvin <= 7499:
+            return "F" 
+        case kelvin if 5200 <= kelvin <= 5999:
+            return "G" 
+        case kelvin if 3700 <= kelvin <= 5199:
+            return "K" 
+        case kelvin if 0 <= kelvin <= 3699:
+            return "M" 
+        case _:
+            return "Error: Something went wrong somewhere."
+
+if __name__ == "__main__":
+    print(classification(9999))
+
+
+
+
+
