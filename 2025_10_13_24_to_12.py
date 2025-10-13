e769b39ca336641bb2ae810d80a75d3ada306445
Tracy_Chacon <tracy.chacon.00@gmail.com>
2025-10-13 11:44:52 -0500

    chore: refac for clarity 2025_10_13 24_to_12

--- 2025_10_13_24_to_12.py
+++ 2025_10_13_24_to_12.py
@@ -4,9 +4,6 @@
 # The given input will always be a four-digit string in 24-hour time format, from "0000" to "2359".
 
 def to_12(time: str) -> str:
-    print(f"24hr: {time}")
-
-    AM_START_DELIMITER = 0
     AM_END_DELIMITER = 1159
 
     meridiem = ""
@@ -14,20 +11,14 @@ def to_12(time: str) -> str:
     hour = time_24_int // 100
     minute = time[2:]
 
-   
-
-    if AM_START_DELIMITER <= time_24_int <= AM_END_DELIMITER:
+    if  time_24_int <= AM_END_DELIMITER:
         meridiem = "AM"
-
         if hour == 0:
             hour = 12
-
-    elif AM_END_DELIMITER < time_24_int:
+    else:
         meridiem = "PM"
         hour -= 12
        
-
-
     return f"{hour}:{minute} {meridiem}"
 
 
@@ -39,3 +30,5 @@ if __name__ == "__main__":
     print(to_12("0030"))
     print(to_12("0009"))
     print(to_12("0000"))
+    print(to_12("1500"))
+    print(to_12("0300"))
