fd6cdedf1c154f094e05f18cc90fd01902f8a080
Tracy_Chacon <tracy.chacon.00@gmail.com>
2025-10-15 07:24:23 -0500

    solve: 2025-10-15 fcc_daily_challenge  html_tag_stripper

--- 2025_10_15_fcc_daily_html_tag_stripper.py
+++ 2025_10_15_fcc_daily_html_tag_stripper.py
@@ -7,7 +7,18 @@
 # For example, '<a href="#">Click here</a>' should return "Click here".
 
 def strip_tags(html: str) -> str:
-    return html
+    in_text = True
+    plain_text_content = ""
+
+    for char in html:
+        if char == "<":
+            in_text = False
+        elif char == ">":
+            in_text = True
+        elif in_text:
+            plain_text_content = plain_text_content + char
+
+    return plain_text_content
 
 if __name__ == "__main__":
     print(strip_tags(""))
