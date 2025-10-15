d5d58af0117b177a1222e565d1fb143528f3a3b4
Tracy_Chacon <tracy.chacon.00@gmail.com>
2025-10-15 07:19:41 -0500

    solve: 2025-10-15 fcc_daily_challenge html_tag_stripper

---
+++ 2025_10_15_fcc_daily_html_tag_stripper.py
@@ -0,0 +1,13 @@
+# HTML Tag Stripper
+# Given a string of HTML code, remove the tags and return the plain text content.
+
+# The input string will contain only valid HTML.
+# HTML tags may be nested.
+# Remove the tags and any attributes.
+# For example, '<a href="#">Click here</a>' should return "Click here".
+
+def strip_tags(html: str) -> str:
+    return html
+
+if __name__ == "__main__":
+    print(strip_tags(""))
