18361fea9793dade8df75779c5373bdca1f038c9
Tracy_Chacon <tracy.chacon.00@gmail.com>
2025-10-17 08:31:41 -0500

    solve: 2025-10-17 fcc_daily_challenge credit_card_masker

---
+++ 2025_10_17_fcc_daily_credit_card_masker.py
@@ -0,0 +1,32 @@
+# Credit Card Masker
+# Given a string of credit card numbers, return a masked version of it using the following constraints:
+
+# The string will contain four sets of four digits (0-9), with all sets being separated by a single space, or a single hyphen (-).
+# Replace all numbers, except the last four, with an asterisk (*).
+# Leave the remaining characters unchanged.
+# For example, given "4012-8888-8888-1881" return "****-****-****-1881".
+def mask(card: str) -> str | None:
+    print(card)
+    EXPECTED_STRING_LENGTH = 19
+
+    if len(card) != EXPECTED_STRING_LENGTH:
+        return "Invalid String"
+    
+    delimeter = card[4]
+    
+    if delimeter not in (' ', '-'):
+        return "Invalid delimiter"
+    
+    last_four = card[15: 19]
+    mask = f"****{delimeter}****{delimeter}****{delimeter}"
+
+    return mask + last_four
+
+
+    
+
+if __name__ == "__main__":
+    print(mask("4012-8888-8888-1881"))
+    print(mask("5105 1051 0510 5100"))
+    print(mask("6011 1111 1111 1117"))
+    print(mask("2223-0000-4845-0010"))
