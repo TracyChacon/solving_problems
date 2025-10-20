64899ff6c20ac32b7e85081cd6c9057364dff549
Tracy_Chacon <tracy.chacon.00@gmail.com>
2025-10-19 23:04:33 -0500

    solve: 2025-10-19 fcc_daily_challenge html_attribute extractor

--- 2025_10_19_fcc_daily_html_attribute_extractor.py
+++ 2025_10_19_fcc_daily_html_attribute_extractor.py
@@ -6,17 +6,34 @@
 # Return an array of strings with each attribute property and value, separated by a comma, in this format: ["attribute1, value1", "attribute2, value2"].
 # Return attributes in the order they are given.
 # If no attributes are found, return an empty array.
+import re
 
 def extract_attributes(element: str) -> list[str]:
-    print(element)
-    start_of_attributes = element.find(' ', element.find('<'))
     
-    print(start_of_attributes)
-    return ["str"]
+    start_of_attributes = element.find(' ', element.find('<'))
+
+    if start_of_attributes == -1 or element[start_of_attributes:].strip().startswith('>'):
+        return []
+  
+    attribute_string = element[start_of_attributes : element.rfind('>')]
+
+
+    attribute_pattern = re.compile(r'(\S+)\s*=\s*\"([^\"]*)\"')
+    
+
+    matches = attribute_pattern.findall(attribute_string)
+    
+  
+    formatted_attributes = []
+    for attr, value in matches:
+      
+        formatted_attributes.append(f"{attr}, {value}")
+        
+    return formatted_attributes
 
 if __name__ in "__main__":
     # should return ["class, red"].
-    print(extract_attributes('< span class="red"></>'))
+    print(extract_attributes('<span class="red"></span>'))
     # # should return ["charset, UTF-8"].
     # print(extract_attributes('<meta charset="UTF-8" />'))
     # # should return []
