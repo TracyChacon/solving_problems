5a544386e4713c7ce5c7d2fd26cfc888d678c480
Tracy_Chacon <tracy.chacon.00@gmail.com>
2025-10-21 21:16:37 -0500

    solve: 2025-10-21 fcc_daily thermosta_adjuster_2

---
+++ 2025_10_21_fcc_daily_thermostat_adjuster_2.py
@@ -0,0 +1,34 @@
+# Thermostat Adjuster 2
+# Given the current temperature of a room in Fahrenheit and a target temperature in Celsius, return a string indicating how to adjust the room temperature based on these constraints:
+
+# Return "Heat: X degrees Fahrenheit" if the current temperature is below the target. With X being the number of degrees in Fahrenheit to heat the room to reach the target, rounded to 1 decimal place.
+# Return "Cool: X degrees Fahrenheit" if the current temperature is above the target. With X being the number of degrees in Fahrenheit to cool the room to reach the target, rounded to 1 decimal place.
+# Return "Hold" if the current temperature is equal to the target.
+# To convert Celsius to Fahrenheit, multiply the Celsius temperature by 1.8 and add 32 to the result (F = (C * 1.8) + 32).
+
+def convert_temp_c_to_f(temp_c: float) -> float:
+    return (temp_c * 1.8) + 32
+
+def adjust_thermostat(current_f: float, target_c: float) -> str:
+    target_f = convert_temp_c_to_f(target_c)
+
+    if current_f < target_f:
+        heat_to_temp_f = target_f - current_f
+        return f"Heat: {heat_to_temp_f:.1f} degrees Fahrenheit"
+    elif current_f > target_f:
+        cool_to_temp_f = current_f - target_f
+        return f"Cool: {cool_to_temp_f:.1f} degrees Fahrenheit"
+    else:
+        return f"Hold"
+     
+if __name__ == "__main__":
+    # should return "Hold".
+    print(adjust_thermostat(32, 0)) 
+    # should return "Heat: 7.0 degrees Fahrenheit".
+    print(adjust_thermostat(70, 25)) 
+    # should return "Cool: 7.6 degrees Fahrenheit".
+    print(adjust_thermostat(72, 18)) 
+    # should return "Hold".
+    print(adjust_thermostat(212, 100)) 
+    # should return "Heat: 12.6 degrees Fahrenheit".
+    print(adjust_thermostat(59, 22)) 
