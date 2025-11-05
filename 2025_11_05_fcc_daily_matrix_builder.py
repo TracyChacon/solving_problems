b5e9bd273acf2cadf12992d139a04be0dee5b5d2
tracy <tracy.chacon.00@gmail.com>
2025-11-05 14:44:59 -0600

    solve: 2025-11-25 fcc_daily matrix_builder

---
+++ 2025_11_05_fcc_daily_matrix_builder.py
@@ -0,0 +1,21 @@
+
+"""
+Given two integers (a number of rows and a number of columns), return a matrix (an array of arrays) filled with zeros (0) of the given size.
+
+For example, given 2 and 3, return:
+
+[
+  [0, 0, 0],
+  [0, 0, 0]
+]
+"""
+
+def build_matrix(rows: int, cols: int) -> list[[int]]:
+    return [[0 for _ in range(cols)] for _ in range(rows)] 
+
+# Tests
+
+print(build_matrix(2, 3)) # should return [[0, 0, 0], [0, 0, 0]].
+print(build_matrix(3, 2)) # should return [[0, 0], [0, 0], [0, 0]].
+print(build_matrix(4, 3)) # should return [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]].
+print(build_matrix(9, 1)) # should return [[0], [0], [0], [0], [0], [0], [0], [0], [0]].
