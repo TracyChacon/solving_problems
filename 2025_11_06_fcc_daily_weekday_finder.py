6d0bf63bd2bc9f37fa9fa3f822b191a61119bf9f
tracy <tracy.chacon.00@gmail.com>
2025-11-06 15:04:21 -0600

    solve: 2025-11-06 fcc_daily weekday_finder

---
+++ 2025_11_06_fcc_daily_weekday_finder.py
@@ -0,0 +1,36 @@
+# Weekday Finder
+
+# Given a string date in the format YYYY-MM-DD, return the day of the week.
+
+# Valid return days are:
+
+#     "Sunday"
+#     "Monday"
+#     "Tuesday"
+#     "Wednesday"
+#     "Thursday"
+#     "Friday"
+#     "Saturday"
+
+# Be sure to ignore time zones.
+
+from datetime import datetime
+
+def get_weekday(date_string: str) -> str:
+    date = datetime.strptime(date_string, '%Y-%m-%d')
+    weekday_full_name = date.strftime('%A')
+
+    return weekday_full_name
+
+# Tests
+
+# Test 1 should return Thursday.
+print(get_weekday("2025-11-06"))
+# Test 2 should return Friday.
+print(get_weekday("1999-12-31"))
+# Test 3 should return Saturday.
+print(get_weekday("1111-11-11"))
+# Test 4 should return Wednesday.
+print(get_weekday("2112-12-21"))
+# Test 5 should return Monday.
+print(get_weekday("2345-10-01"))
