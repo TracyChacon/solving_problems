72c7233bc4f5d46defde4d5d30ff1c925d2e8d9f
tracy <tracy.chacon.00@gmail.com>
2025-11-21 09:51:57 -0600

    chore: add dunder idiom to 2025_11_20_fcc_daily...

--- 2025_11_20_fcc_daily_longest_word.py
+++ 2025_11_20_fcc_daily_longest_word.py
@@ -19,17 +19,19 @@ def longest_word(sentence: str) -> str:
     # "letters only" rule) and, crucially, returns the first word in the list 
     # that ties for the maximum length. (O(N))
     return max(stripped_words, key=len) 
-# Tests
 
-# Test 1 should return "quick".
-print(longest_word("The quick red fox"))
-# Test 2 should return "challenge".
-print(longest_word("Hello coding challenge."))
-# Test 3 should return "This".
-print(longest_word("Do Try This At Home."))
-# Test 4 should return "exlamation".
-print(longest_word("This sentence... has commas, ellipses, and an exlamation point!"))
-# Test 5 should return "tie".
-print(longest_word("A tie? No way!"))
-# Test 6 should return "Wouldnt".
-print(longest_word("Wouldn't you like to know."))
+if __name__ == '__main__':
+    # Tests
+
+    # Test 1 should return "quick".
+    print(longest_word("The quick red fox"))
+    # Test 2 should return "challenge".
+    print(longest_word("Hello coding challenge."))
+    # Test 3 should return "This".
+    print(longest_word("Do Try This At Home."))
+    # Test 4 should return "exlamation".
+    print(longest_word("This sentence... has commas, ellipses, and an exlamation point!"))
+    # Test 5 should return "tie".
+    print(longest_word("A tie? No way!"))
+    # Test 6 should return "Wouldnt".
+    print(longest_word("Wouldn't you like to know."))
