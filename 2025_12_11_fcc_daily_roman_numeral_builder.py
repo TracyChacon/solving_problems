d8361fa4e75fd836f534f198529fc627b893b09d
tracy <tracy.chacon.00@gmail.com>
2025-12-11 12:07:17 -0600

    chore: add greedy solution

--- 2025_12_11_fcc_daily_roman_numeral_builder.py
+++ 2025_12_11_fcc_daily_roman_numeral_builder.py
@@ -20,7 +20,11 @@
 
 # Here's one more example: given 1464, return "MCDLXIV" (1000 + (500 - 100) + 50 + 10 + (5 - 1)).
 
+# Implements rules for conversion for all numbers 1 up to 3999
 def to_roman(num: int) -> str:
+    if not 1 <= num <= 3999:
+        raise ValueError("Input number must be between 1 and 3999")
+    # Positional lookup table grouping by 1s, 10s, 100s, and 1000s and maps each digit's value directly to its corresponding Roman numeral.
     roman_numeral_by_base_10_position = {
     0: ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'],
     1: ['', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC'],
@@ -28,15 +32,49 @@ def to_roman(num: int) -> str:
     3: ['', 'M', 'MM', 'MMM'],}
   
     roman_numeral = []
+    # The integer `num` is converted to a string `num_str`
+    # in order to iterate over later in the for loop
     num_str = f"{num}"
-    position = len(num_str) - 1
+    # The `power` variable tracks the current base 10 power (3 for thousands, 2 for hundreds, 1 for tens, and 0 for ones)
+    power = len(num_str) - 1
 
-    
-    for i in range(position + 1): 
-        roman_numeral.append(roman_numeral_by_base_10_position[position][int(num_str[i])])
-        position -= 1
+    for i in range(power + 1): 
+        # The `number` varible used to as index to fetch the Roman numeral 
+        # representation for that place
+        number = int(num_str[i])
+
+        roman_numeral.append(roman_numeral_by_base_10_position[power][number])
+        power -= 1
 
     return "".join(roman_numeral)
+
+
+# def to_roman(num: int) -> str:
+#     # Key: Value, Value: Roman Symbol
+#     roman_map = {
+#         1000: 'M', 900: 'CM', 500: 'D', 400: 'CD',
+#         100: 'C', 90: 'XC', 50: 'L', 40: 'XL',
+#         10: 'X', 9: 'IX', 5: 'V', 4: 'IV', 1: 'I'
+#     }
+    
+#     result = []
+    
+#     # Iterate from largest value (1000) down to smallest (1)
+#     for value, symbol in roman_map.items():
+#         # Keep track of how many times this value can be subtracted
+#         count = num // value 
+        
+#         # Append the symbol that many times
+#         result.append(symbol * count)
+        
+#         # Subtract the total amount from the number
+#         num -= value * count
+        
+#         # Optimization: If num is 0, we are done
+#         if num == 0:
+#             break
+            
+#     return "".join(result)
 
 if __name__ == '__main__':
     # Tests
